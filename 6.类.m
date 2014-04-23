e3ae01a18252ea5572a9f3eb4c950f27aa391ceb
flyher <dong3580@gmail.com>
2014-04-23 14:47:23 +0800

    update

--- 6.类.m
+++ 6.类.m
@@ -70,10 +70,46 @@ int main(int argc,char *argv[])
 #import <Foundation/Foundation.h>
 //定义Fraction类
 @interface Fraction:NSObject
-@property int numerator,denominator;
+@property int a,b;
+-(void) print;
+-(void) setTo:(int) x over:(int) y;
+-(double) convertToNum;
 @end
-
-
+//代码清单 7-2 实现文件:Fraction.m
+#import "Fraction.h"
+@implementation Fraction
+@synthesize a,b;
+-(void) print
+{
+	NSLog(@"%i/%i",a,b);
+}
+-(double) convertToNum
+{
+	if(b!=0)
+		return (double)a/b;
+	else
+		return NAN;
+}
+-(void) setTo:(int) x over:(int) y
+{
+	a=x;
+	b=y;
+}
+@end
+#代码清单7-2  测试文件 main.m
+#import "Fraction"
+int main(int argc,char *argv[])
+{
+	@autoreleasepool{
+		Fraction *myfra=[[Fraction alloc] init];
+		[myfra setTo:100 over:200];
+		[myfra print];
+		
+		[myfra setTo:1 over:3];
+		[myfra print];
+	}
+	return 0;
+}
 
 
 
