d1a0a64ab475bbde5f7e871c4081001c04dbc9e1
flyher <dong3580@gmail.com>
2014-04-30 16:46:17 +0800

    update

---
+++ 9_1main.m
@@ -0,0 +1,40 @@
+//共享的方法名:多态性
+#import "Fraction.h"
+#import "9_1Complex.h"
+int main(int argc,char *argv[])
+{
+	@autoreleasepool{
+		Fraction *f1=[[Fraction alloc] init];
+		Fraction *f2=[[Fraction alloc] init];
+		Fraction *fracResult;
+		Complex *c1=[[Complex alloc] init];
+		Complex *c2=[[Complex alloc] init];
+		Complex *compResult;
+		[f1 setTo:1 over:10];
+		[f2 setTo:2 over:15];
+		
+		[c1 setReal:18.0 andImaginary:2.5];
+		[c2 setReal:-5.0 andImaginary:3.2];
+		
+		//将两个Complex数相加并显示
+		[c1 print];NSLog(@" +");[c2 print];
+		NSLog("@------");
+		compResult=[c1 add:c2];
+		[compResult print];
+		NSLog(@"\n");
+		//将两个分数相加并显示
+		[c1 print];NSLog(@" +");[f2 print];
+		NSLog(@"----");
+		fracResult=[f1 add:f2];
+		[fracResult print];
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
