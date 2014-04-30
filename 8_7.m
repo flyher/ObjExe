1d41d8c94b6ade7ae81b4138382388c5ede46a67
flyher <dong3580@gmail.com>
2014-04-30 10:30:52 +0800

    update

---
+++ 8_7.m
@@ -0,0 +1,15 @@
+#import <Foundation/Foundation.h>
+//在这里插入ClassA和ClassB的定义
+int main(int argc,char *argv[])
+{
+	@autoreleasepool{
+		ClassA *a=[[ClassA alloc] init];
+		ClassB *b=[[ClassB alloc] init];
+		
+		[a initVar];//使用ClassA方法
+		[a printVar];//显示x的值
+		
+		
+	}
+	return 0;
+}
