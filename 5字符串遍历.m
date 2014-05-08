8cec1515ae814389237ad391e09c6ecfcd240a08
flyher <dong3580@gmail.com>
2014-05-08 13:51:53 +0800

    update

--- 5字符串遍历.m
+++ 5字符串遍历.m
@@ -1,7 +1,7 @@
 int count=[str6length];
 	NSLog(@"字符串长度:%d",count);
 	for(int i=0;i<count;i++){
-		NSString* abc=[str6 substringWithRange:NSMakeRange(i,1)];
+		NSString *abc=[str6 substringWithRange:NSMakeRange(i,1)];
 		NSLog(@"%@",abc);
 	}
 	for(int i=0;i<count;i++){
@@ -12,3 +12,10 @@ int count=[str6length];
 		unichar c=[str6 characterAtIndex:i];
 		NSLog(@"中文情况=%C",c);
 	}
+	//第一种:NSString *abc=[str6 substringWithRange:NSMakeRange(i,1)];
+	//第二种:unichar c=[str6 characterAtIndex:i];
+	
+	//必须使用%C 才能正常输出中文
+	
+	//比较字符串
+	NSString* str1
