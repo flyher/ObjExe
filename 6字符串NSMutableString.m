846b30b1d084310c973a9480c2e581d692343c2a
flyher <dong3580@gmail.com>
2014-05-09 10:17:48 +0800

    update

--- 6字符串NSMutableString.m
+++ 6字符串NSMutableString.m
@@ -1,7 +1,16 @@
 //1.添加字符串
 NSMutableString *str=[[NSMutableString alloc] init];
-[str appendString:@"NSMutableString 字符串"];//类似StringBuild
+[str appendString:@"NSMutableString 字符串"];//类似C#的StringBuild
 NSLog(@"appendString=%@",str);
 
 [str appendString:@" --我的名字:%@ ,年龄 %d",@"Object-C",10];
 NSLog(@"appendFormat:%@",str);
+
+//2.删除字符串
+NSMutableString *str=[NSMutableString stringWithString:@"Object-c之NSMutableString类"];
+NSLog(@"stringWithString=%@",str);
+
+NSRange range=[str rangeOfString:@"Mutable"];
+NSLog(@"Location=%d",rangle.location);
+NSLog(@"length=%d",range.length);
+[str deleteCharactersInRange:range];
