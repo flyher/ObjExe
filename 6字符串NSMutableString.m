8ce6b78181e6e77cf845c0cc23d0a1d6f57a9e4d
flyher <dong3580@gmail.com>
2014-05-09 11:58:53 +0800

    update

--- 6字符串NSMutableString.m
+++ 6字符串NSMutableString.m
@@ -25,10 +25,29 @@ NSString *str2=@"学习";
 NSLog(@"insertString=%@",str);
 
 //4.字符串拷贝
+//创建字符串
 NSMutableString *str1=[NSMutableString stringWithString:@"字符串1"];
+NSMutableString *str2;
+//字符串赋值
+str2=str1;
+[str2 appendString:@" 和字符串2"];
+NSLog(@"str1=%@",str1);
+BSLog(@"str2=%@",str2);
+//5.字符串类型转换
+NSString *str1=@"123";
+int value1=[str1 intValue];
+NSLog(@"转化为int类型:str1=%d",value1);
 
+NSString *str2=@"1234";
+NSInteger value2=[str2 integerValue];
+NSLog(@"转化为NSInteger类型:str2=%i",value2);
 
+NSString *str3=@"3.1415";
+float value3=[str3 floatValue];
+NSLog(@"转换为float类型:str3=%0.4f",value3);
 
+double value4=[str3 floatValue];
+NSLog(@"转换为double类型:str3=%0.4f",value4);
 
 
 
