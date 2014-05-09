a2f861a85920559e24c1ec89258f28164025411c
flyher <dong3580@gmail.com>
2014-05-09 10:05:57 +0800

    update

--- 6字符串NSMutableString.m
+++ 6字符串NSMutableString.m
@@ -1,2 +1,7 @@
 //1.添加字符串
+NSMutableString *str=[[NSMutableString alloc] init];
+[str appendString:@"NSMutableString 字符串"];//类似StringBuild
+NSLog(@"appendString=%@",str);
 
+[str appendString:@" --我的名字:%@ ,年龄 %d",@"Object-C",10];
+NSLog(@"appendFormat:%@",str);
