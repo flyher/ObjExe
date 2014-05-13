144018fcd6652c0396161653be788d52514620ae
flyher <dong3580@gmail.com>
2014-05-13 14:27:12 +0800

    update

--- 2_2.m
+++ 2_2.m
@@ -33,5 +33,19 @@ int main(int argc,const char *argv[])
 	NSLog(@"are %d and %d different?%@",23,42,boolStr(a));
 	return (0);
 }
+//Êä³ö1-5µÄÊý×Ö
+#import <Foundation/Foundation.h>
+int main(int argc,const char *argv[])
+{
+	NSLog(@"The numbers from 1 to 5:");
+	int i;
+	for(i=1;i<=5;i++){
+		NSLog(@"%d\n",i);
+	}
+	return (0):
+}
+
+
+
 
 
