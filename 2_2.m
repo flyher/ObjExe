a22d2459f3e5b3033f409024638e4d25d15041f2
flyher <dong3580@gmail.com>
2014-05-13 11:01:24 +0800

    update

--- 2_2.m
+++ 2_2.m
@@ -4,3 +4,39 @@ int main(int argc,const char *argv[])
 	NSLog(@"Hello,Object-c");
 	return (0);
 }
+//
+#import <Foundation/Foundation.h>
+BOOL strTobool(int thing1,int things)
+{
+	if(thing1==thing2){
+		return (NO);
+	}else{
+		return (YES);
+	}
+}
+
+NSString *boolStr(BOOL yesNo)
+{
+	if(yesNo==No){
+		return (@"NO");
+	}else{
+		return (@"YES");
+	}
+}
+
+int main(int argc,const char *argv[])
+{
+	BOOL a;
+	a=strTobool(5,5);
+	NSLog(@"are %d and %d different?%@",5,5,boolStr(a));
+	a=strTobool(23,42);
+	NSLog(@"are %d and %d different?%@",23,42,boolStr(a));
+	return (0);
+}
+
+
+
+
+
+
+
