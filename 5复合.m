5abd053f1dd0b8b5697abd59a51cabdd7dc2c806
flyher <dong3580@gmail.com>
2014-05-19 14:12:58 +0800

    update

---
+++ 5复合.m
@@ -0,0 +1,26 @@
+//¸´ºÏ
+@interface Unicycle:NSObject
+{
+	Pedal *pedal;
+	Tire *tire;
+}
+@end
+////
+#import <Foundation/Foundation.h>
+@interface Tire:NSObject
+@end //Tire
+@implementation Tire
+-(NSString *) description{
+	return (@"i am a tire.i last a while.");
+} //description
+@end //Tire
+////
+
+@interface Engine:NSObject
+@end //Engine
+@implementation Engine
+-(NSString *) description
+{
+	return (@"i am an engine,Vroom!");
+} //description
+@end //Engine
