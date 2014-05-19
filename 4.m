d4bc0041cb43f50264218b82d48e2acfaf3c36e8
flyher <dong3580@gmail.com>
2014-05-19 11:59:23 +0800

    update

--- 4.m
+++ 4.m
@@ -77,7 +77,18 @@
 	NSLog(@"drawing rect at (%d %d %d %d) in %@",bounds.x,bounds.y,bounds.width,bounds.height,colorName(fillColor));
 }
 @end
-
+////
+//修改基类的方法(改颜色)
+@implementation Circle
+-(void) setFillColor:(ShapeColor) c
+{
+	if(c==kRedColor)
+	{
+		c=kGreenColor;
+	}
+	[super setFillColor:c];
+}
+@end
 
 
 
