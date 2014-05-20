85c2fb9602945d595af1c5e20225a4920c774600
flyher <dong3580@gmail.com>
2014-05-20 10:48:54 +0800

    update

--- 5复合.m
+++ 5复合.m
@@ -33,9 +33,27 @@
 @end //Car
 
 //新建Car对象
-
-
-
+@implementation Car
+-(id) init
+{
+    if(self=[super init]){
+        engine=[Engine new];
+        tires[0]=[Tire new];
+        tires[1]=[Tire new];
+        tires[2]=[Tire new];
+        tires[3]=[Tire new];
+    }
+    return (self);
+} //init
+-(void) print
+{
+    NSLog(@"%@",engine);
+    NSLog(@"%@",tires[0]);
+    NSLog(@"%@",tires[1]);
+    NSLog(@"%@",tires[2]);
+    NSLog(@"%@",tires[3]);
+} //print
+@end //Car
 
 
 
