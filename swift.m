1b39290148ee1bdc59902ed06d37e16ecd49ca7d
flyher <dong3580@gmail.com>
2014-06-06 14:28:07 +0800

    update

---
+++ swift.m
@@ -0,0 +1,11 @@
+//hello
+println("hello,world")
+//¼òµ¥¸³Öµ
+var myVariable=42
+myVariable=50
+let myConstant=42
+//
+let implicitInteger =70
+let implicitDouble=70.0
+let explicitDouble:Double=70;
+//Á·Ï°:
