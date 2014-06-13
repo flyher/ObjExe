2b9b7e64575cd608ffbb6c47127b9568fb2158c3
flyher <dong3580@gmail.com>
2014-06-13 13:32:52 +0800

    update

--- swift.m
+++ swift.m
@@ -38,6 +38,25 @@ shoppingList=[]
 ////////////////控制流
 //条件操作
 // if,switch,for-in,for,while和do-while
+let individualScores=[75,43,103,87,12]
+var teamScore=0
+for score in individualScores{
+    if score>50{
+        teamScore+=3
+    }else{
+        teamScore+=1
+    }
+}
+teamScore
+// ? 标记类型可选
+var optionalString:String?="Hello"
+optionalString==nil
+
+var optionalName:String?="John Appleseed"
+var greeting="Hello!"
+if let name=optionalName{
+    greeting="Hello,\(name)"
+}
 
 
 
